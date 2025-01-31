a76aab837e303fa70dbfd3aaf969e295bc90008e
terrencethe1 <terrence.jsr@gmail.com>
2025-01-31 10:40:28 -0600

    implement login function to make POST request to login route

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,28 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
 
+    if (!response.ok) {
+      throw new Error('Failed to login');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
 
 
 export { login };
+
+
